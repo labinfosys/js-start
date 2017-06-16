ec0ee25de6f16ff9242c17afe5375dcf3f055cd2
Sergey Karaichev <sk@labinfosys.ru>
2017-06-16 17:17:42 +0300

    Генерация случайного числа и заполнение списка выпавших чисел вынесено в отдельный объект

--- js/main.js
+++ js/main.js
@@ -3,32 +3,43 @@ var currentChipsValue = null;
 var accountVal = 500;
 var rateVal = 0;
 
-function getNumber() {
-    return Math.round(Math.random() * 36);
-}
 
-function getColor(num) {
-    var black = [2,4,6,8,10,11,13,15,17,22,24,26,28,29,31,33,35];
-    if (num == 0) 
-        return '';
-    if (black.indexOf(num) >= 0)
-        return 'black';
-    return 'red';
-}
+var numberList = (function() {
+    var list;
+    var numbers = [];
+    var getNumber = function() {
+        return Math.round(Math.random() * 36);
+    };
+    var getColor = function(num) {
+        var black = [2,4,6,8,10,11,13,15,17,22,24,26,28,29,31,33,35];
+        if (num == 0) 
+            return 'green';
+        if (black.indexOf(num) >= 0)
+            return 'black';
+        return 'red';
+    };
+    var draw = function() {
+        list.forEach(function(el, i) {
+            if (numbers[i] !== undefined) {
+                el.className = 'number number--' + getColor(numbers[i]);
+                el.innerHTML = numbers[i];
+            }
+        });
+    };
+    return {
+        init: function() {
+            list  = document.querySelectorAll('.number-list > .number');
+        },
+        roll: function() {
+            var num = getNumber();
+            numbers.unshift(num);
+            if (numbers.length > 10) numbers.pop();
+            draw();
+        }
+    }
+})();
 
-function getName(num) {
-    if (num == 0) 
-        return 'zero';
-    return num + ' ' + getColor(num);
-}
-
-function addNumber() {
-    var num = getNumber();
-    nums.unshift(getName(num));
-    if (nums.length > 10) 
-        nums.pop();
-    console.log(nums);
-}
+numberList.init();
 
 function getChildByClassName(el, className) {
     var children = [];
@@ -95,7 +106,7 @@ document.addEventListener('DOMContentLoaded', function() {
     btnStart.addEventListener('click', function() {
         clearRates();
         deactivateChips();
-        addNumber();
+        numberList.roll();
         rateVal = 0;
         updateInfo();
     });
